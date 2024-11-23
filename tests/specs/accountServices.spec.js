42b16e31efdb033606d7411101936809622bbbef
Anand Kedari <anandkedari@yahoo.com>
2024-11-24 03:27:16 +0530

    Anand | Add dummy test with description, to be implemented soon

---
+++ tests/specs/accountServices.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test } from '../common/fixtures';
+
+test.describe("Customer ", () => {
+    test("is able to open new account", async({ page }) => {
+    });
+    test("is displayed account balances", async({ page }) => {
+    });
+    test("is able to transfer funds from newly created account", async({ page }) => {
+    });
+    test("is able to pay bills from newly created account", async({ page }) => {
+    });
+});
---
+++ tests/specs/navigation.spec.js
@@ -0,0 +1,7 @@
+// @ts-check
+import { test } from '../common/fixtures';
+
+test.describe("Customer ", () => {
+    test("is redirected to respective page on clicking navigation menu", async({ page }) => {
+    });
+});
