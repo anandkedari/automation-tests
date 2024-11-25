f59c38a0df4b15673fcf24d545505ae31d52fe81
Anand Kedari <anandkedari@yahoo.com>
2024-11-25 22:56:48 +0530

    Anand | Pushing navigation journey tests

--- tests/specs/navigation.spec.js
+++ tests/specs/navigation.spec.js
@@ -1,7 +1,23 @@
 // @ts-check
-import { test } from '../common/fixtures';
+import { test, expect } from '../common/fixtures';
+import { homePage } from '../pages/home.page';
+import data from '../common/testdata';
+import { dashboardPage } from '../pages/dashboard.page';
 
 test.describe("Customer ", () => {
+    let testData = data.get.registered_user;
     test("is redirected to respective page on clicking navigation menu", async({ page }) => {
+        expect(await (await (await (await 
+            homePage.navigateToHomePage())
+            .login(testData))
+            .clickHomeIcon())
+            .isAccServicesSectionVisible()).toBe(true);
+        expect(await (await 
+            dashboardPage.clickOnAboutUsIcon())
+            .isAboutUsSectionVisible()).toBe(true);
+        expect(await (await 
+            dashboardPage.clickOnContactIcon())
+            .isContactFormVisible()).toBe(true); 
+        await dashboardPage.logOut();
     });
 });
