fb27c18c0a94bc80cba9e0461e2988e01f82bbd7
Anand Kedari <anandkedari@yahoo.com>
2024-11-27 00:06:00 +0530

    Anand | Add assertions to api test

--- tests/specs/api/transactions.spec.js
+++ tests/specs/api/transactions.spec.js
@@ -4,11 +4,13 @@ import {crossTestSharedData} from '../../common/constants';
 
 test.describe('API Tests - ', () => {
     test('Find transaction by amount', async ({ request, sharedData }) => {
-      console.log("sharedData - ", crossTestSharedData);
+      // console.log('sharedData - ' + JSON.stringify(await sharedData));
       const response = await request.get('https://parabank.parasoft.com/parabank/services_proxy/bank/accounts/16674/transactions/amount/11', {
                 Headers:{ 'Cookie': await getToken(request) }
             });
-
-      console.log("JSON - response -====> " + await response.body());
+      const body = await response.json();
+      expect(response.status()).toBe(200);
+      expect(body).toHaveProperty('amount');
+      expect(body.amount).toBe('100.00');
     });
 });
